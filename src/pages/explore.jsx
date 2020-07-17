358d51c7dcf24a93b838bc8356666d8ec9d2d47b
Hao Tien Pan <haotienpan@outlook.com>
2020-07-16 20:28:21 -0400

    add new explore page, no form yet

---
+++ src/pages/explore.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+// import UserForm from '../components/UserForm/UserForm';
+
+export default function Explore() {
+    return (
+        <div>
+            {/* <UserForm/> */}
+            <h1>explore form here</h1>
+        </div>
+    )
+}
