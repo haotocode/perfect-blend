fa41d6be80f95e5e33c47a5451a82caa7e9fcfad
Hao Tien Pan <haotienpan@outlook.com>
2020-07-19 23:39:12 -0400

    added KommunicateChat to main.jsx, no logic yet but works

--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -3,6 +3,7 @@ import Tilt from 'react-tilt';
 import '../styles/main.scss';
 import Layout from '../components/Layout';
 import Nav from '../components/Nav/Nav';
+import KommunicateChat from '../components/Chat';
 
 function Main() {
     return (
@@ -11,6 +12,7 @@ function Main() {
                 <Tilt className="Tilt" options={{ max : 25, scale: 1}}  style={{ height: 100, width: 100 }}>
                     <Nav/>
                 </Tilt>
+                <KommunicateChat/>
             </Layout>
         </div>
     )
