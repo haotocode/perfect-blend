e6046aa4e3705a18841e5dbfbffe0649ded5405a
Hao Tien Pan <haotienpan@outlook.com>
2020-07-21 13:04:19 -0400

    added blog.scss, started styling blog.js

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,44 +3,51 @@ import { graphql } from "gatsby"
 import AniLink from "gatsby-plugin-transition-link/AniLink";
 import Bio from "../components/bio"
 import Layout from '../components/Layout';
-import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
+import SEO from "../components/seo";
+import { rhythm } from "../utils/typography";
+import '../styles/blog.scss';
 
 const BlogIndex = ({ data, location }) => {
   const siteTitle = data.site.siteMetadata.title
   const posts = data.allMarkdownRemark.edges
 
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                // style={{
-                //   marginBottom: rhythm(1 / 4),
-                // }}
-              >
-                <AniLink swipe direction="left" style={{ boxShadow: `none` }} to={node.fields.slug}>
-                  {title}
-                </AniLink>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        )
-      })}
-    </Layout>
+    <section className="blog">
+      <Layout location={location} title={siteTitle}>
+        <div className="blog__main-section">          
+          <SEO title="All posts" />
+          <Bio />
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <article key={node.fields.slug} className="blog__entry">
+                <header className="blog__title-date">
+                  <h3 
+                  className="blog__titles"
+                    // style={{
+                    //   marginBottom: rhythm(1 / 4),
+                    // }}
+                  >
+                    <AniLink swipe direction="left" style={{ boxShadow: `none` }} to={node.fields.slug} className="blog__links">
+                      {title}
+                    </AniLink>
+                  </h3>
+                  <small>{node.frontmatter.date}</small>
+                </header>
+                <section>
+                  <p 
+                  className="blog__excerpt"
+                    dangerouslySetInnerHTML={{
+                      __html: node.frontmatter.description || node.excerpt,
+                    }}
+                  />
+                </section>
+              </article>
+            )
+          })}
+        </div>
+      </Layout>
+    </section>
   )
 }
 
