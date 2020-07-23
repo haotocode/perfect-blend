9d685b7eac6c9b8409180dce596b94f0c988c8a5
Hao Tien Pan <haotienpan@outlook.com>
2020-07-23 15:46:40 -0400

    added recommendation.scss, before adding react items carousel

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import AniLink from "gatsby-plugin-transition-link/AniLink";
+import Typist from 'react-typist';
+import './Recommendation.scss';
 
 export default function Recommendation() {
     return (
-        <div>
-            <h1>Recommendation</h1>
+        <div className="recommendation">
+            <h5>Our Recommendation</h5>
+            <span>image here</span>
+            <p>some kind of description about the coffee matching the preference</p>
             <p>we think you'll enjoy our recommendation.</p>
             <AniLink to='/main' paintDrip duration={1}><button>Home</button></AniLink>
         </div>
